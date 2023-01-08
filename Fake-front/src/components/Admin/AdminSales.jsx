14b9dfcc611db066ccc3a92ad2f795d9b14d8649
Yehoshua Tarshish <a0526741852@gmail.com>
2023-01-08 09:09:41 +0200

    clearing

--- Fake-front/src/components/Admin/AdminSales.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-function AdminSales() {
-  return <div className="admin-sales">AdminSales</div>;
-}
-
-export default AdminSales;
